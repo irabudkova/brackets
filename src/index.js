92f5b0ab61b4e1fdaf670a284ff781ade4e9e33e
irabudkova <thehive@list.ru>
2020-10-23 22:47:18 +0300

    feat: implement brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+      
+        let currentBracket = str[i];
+
+        if (isOpeningBracket(currentBracket, bracketsConfig)) {
+            stack.push(currentBracket);
+        } else {
+            if (stack.length === 0) {
+                return false;
+            }
+
+            let lastBracket = stack.pop();
+            let isMatching = match(lastBracket, currentBracket, bracketsConfig);
+
+            if (!isMatching) {
+                return false;
+            }
+        }
+    }
+
+    if (stack.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isOpeningBracket(bracket, config) {
+    for (let i = 0; i < config.length; i++) {
+        if (bracket === config[i][0]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function match(openingBracket, closingBracket, config) {
+    for (let i = 0; i < config.length; i++) {
+        if (config[i][0] === openingBracket) {
+            if (config[i][1] === closingBracket) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return false;
 }
